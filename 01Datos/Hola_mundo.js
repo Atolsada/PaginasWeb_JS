aaa359e49add0cbae88daf4f07c0e9ce53678c00
Atolsada <Atolsadag@gmail.com>
2018-06-21 13:09:47 +0200

    Creación HOLA_MUNDO

---
+++ 01Datos/Hola_mundo.js
@@ -0,0 +1,21 @@
+/*  var mi-saludo [kebacase]
+    var MiSaludo [Pascalcase]
+    var miSaludo [Camelcase] es lo normal por convenio 
+
+
+var saludo -> declaración de variable 
+saludo="hola" -> inicialización de variable
+
+Declaración + Inicialización
+
+Tipos de datos
+ - string [hola, pepe]
+ - number [2,3,4]
+ - booleans [true / false]
+ - object [lo que no es lo anterior de los anteriores]
+ - undefined [que no esta definido, no vale nada]
+ */
+ 
+var user = "Mundo"
+
+console.log(saludo, user) /* console.log es mostrar en pantalla */
