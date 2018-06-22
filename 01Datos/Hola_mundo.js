f4934d992fbe906e8f6bc289d25f25d5f6804896
Atolsada <Atolsadag@gmail.com>
2018-06-22 09:11:01 +0200

    fix better commits

--- 01Datos/Hola_mundo.js
+++ 01Datos/Hola_mundo.js
@@ -11,9 +11,21 @@ Tipos de datos
  - string [hola, pepe]
  - number [2,3,4]
  - booleans [true / false]
- - object [lo que no es lo anterior de los anteriores]
+ - object [lo que no es lo anterior de los anteriores (un conjunto de propiedades)]
  - undefined [que no esta definido, no vale nada pero si tiene tipo, su tipo es undefined]
- */
+ 
+ **Ejemplos**
+
+ - var user ='Mundo'
+ - var edad = 23
+ - var estudiante = false
+ - var persona = (){
+     nombre: 'pepe',
+     edad: 34,
+     isEstudiante: true,
+ }
+ - var algo 
+*/
  
 var saludo = 'Hola'
 var user = 'Mundo'
