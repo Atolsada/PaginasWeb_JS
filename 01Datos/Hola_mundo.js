65ccc65d400ad87939363461c79b472cad3f81bf
Atolsada <Atolsadag@gmail.com>
2018-06-22 10:01:48 +0200

    fin hola_mundo

--- 01Datos/Hola_mundo.js
+++ 01Datos/Hola_mundo.js
@@ -19,7 +19,7 @@ Tipos de datos
  - var user ='Mundo'
  - var edad = 23
  - var estudiante = false
- - var persona = (){
+ - var persona = (){     <-También llamado, objeto de notación JSON
      nombre: 'pepe',
      edad: 34,
      isEstudiante: true,
@@ -31,3 +31,33 @@ var saludo = 'Hola'
 var user = 'Mundo'
 
 console.log (saludo, user)
+
+
+var nombre ='Mundo'
+var edad = 23
+var isEstudiante = false
+var algo
+var persona = {
+    nombre: 'pepe',
+    edad: 34,
+    isEstudiante: true,
+}
+
+console.log(saludo, user)
+console.log(nombre, typeof nombre)
+console.log(edad, typeof edad)
+console.log(isEstudiante, typeof isEstudiante)
+console.log(persona, typeof persona)
+console.log(persona.nombre, typeof persona.nombre)
+
+
+/** Como es JS respecto a los tipos
+ * - débil ->puedes crear variables y sabes que tipo son sin definirlos, existen tipo undefined
+ * - dinámico ->las variables ya tienen tipo por que hay que definirlo/declararlo, no existen tipo undefined.
+ */
+
+saludo = '22'
+console.log(Number(saludo)*2)
+console.log(typeof saludo)
+var a ='22' //* <-string
+var b = 22 //* <-número
