b832c30123edafd6c6138aea72a83a4f74d52db7
Atolsada <Atolsadag@gmail.com>
2018-06-26 10:29:42 +0200

    Creación Areas.js

---
+++ Areas.js
@@ -0,0 +1,18 @@
+/* Calcular el circulo dado un radio */
+
+/* 3 por ejemplo */
+
+/* PASO 1 */
+function main(){
+    let radio = 3; 
+    calcularCirculo(radio)
+}
+main()
+
+
+/* PASO 2 */
+function main(){
+    let radio = 3; 
+    let circulo = calcularCirculo(radio)
+}
+main()
