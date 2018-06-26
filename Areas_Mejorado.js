74b89017e4466a346b8dd5edee70e559bea97967
Atolsada <Atolsadag@gmail.com>
2018-06-26 11:20:08 +0200

    Está bien pero mirar el del profesor

--- Areas_Mejorado.js
+++ Areas_Mejorado.js
@@ -22,3 +22,18 @@ function main() {
 }
 
 main()
+
+/* -------------------------- */
+
+function calcularCircunf(radio2) {
+    return Math.PI*radio2*radio2
+}
+function mostrarCircunf(radio2, circulo2){
+    let mensaje2 = `El area de la circunferencia ${radio2} es ${circulo2}`
+    console.log(mensaje2)
+}
+function main2(){
+    let radio2 = 4
+    mostrarCircunf(radio2, calcularCircunf(radio2))
+}
+main2()
