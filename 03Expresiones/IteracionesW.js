e06f145702a1c308a22a47a916e0b6a38a623081
Atolsada <Atolsadag@gmail.com>
2018-07-10 12:22:02 +0200

    añadido do while

--- 03Expresiones/IteracionesW.js
+++ 03Expresiones/IteracionesW.js
@@ -1,3 +1,5 @@
+// FOR
+
 function iterar (){
     for (let i = 0;
         i < 3; 
@@ -6,8 +8,12 @@ function iterar (){
     }
 }
 // Por donde empiezo? Por 0. Hasta donde? Hasta 3, cuanto aumento? de 1 en 1.
+// i++ --> i + 1
 
 iterar()
+
+
+//WHILE
 
 function iterar_while(){
     let i = 0
@@ -15,7 +21,19 @@ function iterar_while(){
         console.log(`Soy la vuelta ${i+1} de while`)
         i++
     }
-
 }
 
 iterar_while()
+
+
+// DO WHILE
+
+function iterar_do(){
+    let i = 0
+    do {
+        i++
+        console.log(`Soy la vuelta ${i+1} de do while`)
+    } while (i < 3)
+}
+
+iterar_do()
