9261572ebe6086e4fe35d2174dff9ec909bbfd0d
Atolsada <Atolsadag@gmail.com>
2018-07-10 13:14:09 +0200

    End estadistica

--- 03Expresiones/Estadistica.js
+++ 03Expresiones/Estadistica.js
@@ -10,6 +10,7 @@ console.log (media(2,4,6)) //(2)
 
 //SOLUCION PARA HACER UNA FUNCION DE MEDIA SEA LA CANTIDAD QUE SEA DE VALORES
 
+
 function mediaArray(aDatos){
     //(5)
     let suma = 0
@@ -20,3 +21,38 @@ function mediaArray(aDatos){
     return suma / aDatos.length //(8) para que nso devuelva la solucion
 }
 console.log(mediaArray([2,4,6, 8 ,12])) //(3)
+
+//ARGUMENTS
+// en Js si paso parametros de mas o menos, no da error, da undefined
+//Js tiene "por debajo" arguments
+
+
+function mediaArg(){
+    console.log(arguments)
+    let suma = 0
+    for (let i = 0; i < arguments.length; i++) {
+        suma += arguments[i]
+    }
+    return suma / arguments.length
+}
+
+console.log (mediaArg(2,4,6,8,12))
+
+//OPERADOR DE PROPAGACION - SPREAD
+
+function mediaSpread(...aDatos){
+    let suma = 0
+    for (let i = 0; i < aDatos.length; i++) {
+        suma += aDatos[i]
+    }
+    return suma / aDatos.length
+}
+
+console.log (mediaSpread(2,4,6,8,12))
+
+// calcular entre distinas operaciones y al elijas ¿LOCURON?
+
+function calculaEstadisticos(operacion,...aDatos){
+
+}
+console.log(calculaEstadisticos('varianza',2,4,6,8,12))
