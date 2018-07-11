de6bc205175b7df3d8a3dc53a1e6efa8d0d87716
Atolsada <Atolsadag@gmail.com>
2018-07-11 11:18:20 +0200

    fix temp

---
+++ 05Objetos/watson.js
@@ -0,0 +1,15 @@
+// ELEMENTALES
+
+let x = 23
+let oX = new Number(23)
+
+console.log(typeof x)
+console.log(typeof oX)
+console.log(oX * 2) //<---Object que se comporta como elemental number
+
+// '22' * 2
+// Number ('22') * 2
+
+let oDatos = {}
+let aDatos = []
+
