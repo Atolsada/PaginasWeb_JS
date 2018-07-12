93a08d1786dcb7958de0cfb18ad66a01ff6c1ea2
Atolsada <Atolsadag@gmail.com>
2018-07-12 10:40:13 +0200

    end math dni

--- 05Objetos/math_DNI.js
+++ 05Objetos/math_DNI.js
@@ -1,3 +1,11 @@
 //Ejercicio DNI
 
-let letras = ['T','R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X',	'B', 'N', 'J', 'Z',	'S', 'Q', 'V', 'H',	'L', 'C', 'K', 'E', 'T' ]
+
+function DNI(num) {
+    const LETRAS = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+    return LETRAS [num%23]
+    //const aLETRAS = 'TRWAGMYFPDXBNJZSQVHLCKET'
+    //return aLETRAS [num%23]
+}
+console.log(DNI(48408554))
+console.log(DNI(49321791))
