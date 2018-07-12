a4cb9e3ce913607a704b70db645fd9e3f06e70a7
Atolsada <Atolsadag@gmail.com>
2018-07-12 13:46:07 +0200

    Comprobar splice - no funciona añadir

--- 05Objetos/array.js
+++ 05Objetos/array.js
@@ -26,3 +26,33 @@ aNombres.sort()
 // aNombres.reverse()
 console.log(aNombres)
 console.log(aNombresOriginal)
+
+
+//*SLICE*
+
+dNombres = ['Pepe', 'Luis', 'Arancha', 'Rosa', 'Maria']
+console.log(dNombres.slice(1,4))
+console.log(dNombres)
+
+dClon = dNombres.slice(0,dNombres.length) //<--- El metodo de clonar tipico y comun
+dClon.push('Juan') //<--- Añadimos Juan al clon, no al original
+console.log(dClon)
+let dElegidos = dNombres.splice(2,0,'Antonio') //<--- Para añadirlo en una posicion concreta **NO FUNCIONA**
+
+//*SPLICE*
+
+let aElegidos = aNombres.splice(1,2)
+console.log(aNombres) //<--- [ 'Pepe', 'Rosa', 'Maria' ]
+console.log(aElegidos) //<--- [ 'Luis', 'Arancha' ]
+
+
+bNombres = ['Pepe', 'Luis', 'Arancha', 'Rosa', 'Maria']
+let bElegidos = bNombres.splice(1,2,'Carlos', 'Juan')
+console.log(bNombres)  //<--- [ 'Pepe', 'Carlos', 'Juan', 'Rosa', 'Maria' ]
+console.log(bElegidos) //<--- [ 'Luis', 'Arancha' ]
+
+
+cNombres = ['Pepe', 'Luis', 'Arancha', 'Rosa', 'Maria']
+let cElegidos = cNombres.splice(1)
+console.log(cNombres)  //<--- [ 'Pepe' ]
+console.log(cElegidos) //<--- [ 'Luis', 'Arancha', 'Rosa', 'Maria' ]
