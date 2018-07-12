6671c2302c0b5d6b38219e3c521c1c5a4b91d406
Atolsada <Atolsadag@gmail.com>
2018-07-12 10:09:30 +0200

    Creacion Math

---
+++ 05Objetos/math.js
@@ -0,0 +1,23 @@
+let oDatos = {}
+// oDatos = new Object()
+
+//let x = new Math()
+//ESTO NO EXISTE
+
+Math.PI //<--- numero PI
+
+Math.cos(90) //<--- hacer el coseno de 90
+
+Math.random //<---Numero aleatorio entre 0 y 1, siempre diferentes por llamada
+console.log(Math.random())
+console.log(Math.random())
+console.log(Math.random())
+console.log(Math.random())
+console.log(Math.random())
+
+//Numero aleatorio de 1 al 10 y redondeamos
+console.log(parseInt(Math.random()*10))
+console.log(parseInt(Math.random()*10))
+console.log(parseInt(Math.random()*10))
+console.log(parseInt(Math.random()*10))
+console.log(parseInt(Math.random()*10))
