5dbd00c3dc2cbae45952d8a53576e7de6a2ec50b
Atolsada <Atolsadag@gmail.com>
2018-07-12 09:58:15 +0200

    explicacion - instancia y prototipo

--- 05Objetos/array.js
+++ 05Objetos/array.js
@@ -1,7 +1,9 @@
 // ARRAYS
 
 let oDatos = {}
-// oDatos = new Object()
+// oDatos = new Object() oDatos es "una instancia" del objeto
+// Ejemplo vida real "somos instancias de la clase personas"
+// Pasar de algo abstracto a algo concreto
 
 
 aDatos = []
