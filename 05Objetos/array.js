be33ef66f4abbfe43226b14b829612b6db54839b
Atolsada <Atolsadag@gmail.com>
2018-07-12 12:31:23 +0200

    end array?

--- 05Objetos/array.js
+++ 05Objetos/array.js
@@ -6,8 +6,23 @@ let oDatos = {}
 // Pasar de algo abstracto a algo concreto
 
 
-aDatos = []
+aDatos = [1,2]
 // aDatos = new array()
 
-oDatos.
+console.log(aDatos.length)
+aDatos[10] = 4
 console.log(aDatos)
+console.log(typeof aDatos[7])
+aDatos[7] = 'Pepe'
+aDatos[5] = {}
+
+console.log(aDatos + [3,4])
+console.log(aDatos.concat ( [3,4]))
+
+let aNombres = ['Pepe', 'Luis', 'Arancha', 'Rosa', 'Maria']
+// aNombresOriginal = aNombres <--- Con esto no guardamos la copia
+aNombresOriginal = aNombres.join('|').split('|') //<--- hacemos string el array con el join y luego le aplicamos el split para recuperar el array
+aNombres.sort()
+// aNombres.reverse()
+console.log(aNombres)
+console.log(aNombresOriginal)
