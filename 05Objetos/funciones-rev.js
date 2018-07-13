0e44ba302cc2ff450b6737d3d0e482099b99ea89
Atolsada <Atolsadag@gmail.com>
2018-07-13 09:48:22 +0200

    end funciones-revs

--- 05Objetos/funciones-rev.js
+++ 05Objetos/funciones-rev.js
@@ -5,14 +5,14 @@ basica(y)
 //Siempre se pasan valores (y)(o el que sea) pero son valores
 
 function miFuncion(...z) {    //<--- (Z) es un parametro array, lo que va recogiendo en un array lo que no es array son los (...)
-    console.log('Hola ' + z[0])
+    console.log('Hola ' + z[0]) //<--- (...) se llama splitoperator
     console.log('Hola ' + arguments[0])
 }
 
 let x = 21
-miFuncion(x)
-miFuncion(x, 56)
-miFuncion(x, 56, 89)
+miFuncion(x) // <--- 21 (z -> 1)
+miFuncion(x, 56) // <--- 21, 56 (z -> 2)
+miFuncion(x, 56, 89) // <--- 21, 56, 89 (z -> 3)
 
 
 
