44b4db57c613924f9e1d742de6a9c1bf4880f8b8
Atolsada <Atolsadag@gmail.com>
2018-07-13 11:06:29 +0200

    end temp funciones2B

--- 05funciones/funciones2B.js
+++ 05funciones/funciones2B.js
@@ -7,4 +7,41 @@ function sumaCuadrados(a, b){
     return a*a+b*b
 }
 
+function sumaCubos(a,b){
+    return a*a*a+b*b*b
+}
+
 verResultados(2,3, sumaCuadrados)
+verResultados(2,3, sumaCubos)
+
+verResultados(2,3, function(a,b){return a*a+b*b})
+verResultados(2,3, function(a,b){return a*a*a+b*b*b})
+
+
+// setTimeout() <--- una funcion que quieres que se ejecute y otra cuando quieres que se ejecute
+
+setTimeout(function(){
+    console.log('Han pasado 3 segundos')
+    },3000)
+
+
+function saludar(){
+    console.log ('Hola')
+}
+
+setTimeout(saludar, 2000) //se ejecuta antes que el de 3 segundos por que este es de 2 segundos independientemente del orden
+
+
+setInterval(saludar, 2000) // se ejecuta cada 2 segundos hasta el infinito
+
+// Manera de hacer que pare tras repetirse 10 veces
+
+function saludar() {
+    console.log('Hola');
+    i++
+    if (i > 10) {
+        clearInterval(handler)
+    }
+}
+let i = 0
+let handler = setInterval(saludar, 1000)
