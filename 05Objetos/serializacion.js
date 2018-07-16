8d6c4424966b5cefca1bdfd35ade734dc13bb338
Atolsada <Atolsadag@gmail.com>
2018-07-16 12:35:43 +0200

    Parseos y arreglos

--- 05Objetos/serializacion.js
+++ 05Objetos/serializacion.js
@@ -6,7 +6,9 @@ let oPersona ={     // Funciones que asignamos a propiedades (es la propiedad qu
     }
 }
 
+// oPersona.name *= 2  - es un NaN y da Null
 oPersona.saludar()
+
 
 
 let aDatos = [2, 3, 4, 5]
@@ -17,3 +19,15 @@ console.log(typeof JSON.stringify(oPersona));
 
 console.log('Tengo un string' + JSON.stringify(aDatos));
 console.log(typeof JSON.stringify(aDatos));
+
+
+// Parsear string, PERO NO VALE CUALQUIER STRING
+/* let datosString = JSON.stringify(oPersona) */
+// let datosString = 'Hola, me llamo Pepe'  Ej, string no valido - Unexpected token H in JSON at position 0
+// let datosString = "{name: 'Pepe', edad: 33 }"  Ej, string no valido
+let datosString = '{ "name": "Pepe", "edad": 33 }' // Arreglado para que funcionase este tipo de string
+let oDatos = JSON.parse(datosString)
+console.log(typeof oDatos)
+console.log(oDatos)
+
+
