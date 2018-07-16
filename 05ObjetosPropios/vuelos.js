f1e41416a1782d86c9ab1c7d052c238483fa2cd6
Atolsada <Atolsadag@gmail.com>
2018-07-16 13:07:46 +0200

    Creacion vuelos

---
+++ 05ObjetosPropios/vuelos.js
@@ -0,0 +1,29 @@
+let oVueloVacio = {
+    airline:'',
+    number:'',
+    departure : {
+        clave_IATA : '',
+        time: '',
+        ciudad: ''
+    },
+    arival: {
+        clave_IATA : '',
+        time: '',
+        ciudad: ''
+    }
+}
+
+oVuelo.mostrar = function (oDatos = this) {
+    for (const key in oDatos) {
+        if (oDatos.hasOwnProperty(key) && (typeof oDatos[key] !== 'function')) {
+            if (typeof oDatos[key] === 'object') { 
+                console.log(`Las propiedades de ${key} son: `)
+                this.mostrar(oDatos[key])
+            } else {
+                console.log(`La propiedad ${key} vale ${oDatos[key]}`)
+            }    
+        }    
+    }
+}
+
+let oVuelo1 = Object.create(oVueloVacio)
