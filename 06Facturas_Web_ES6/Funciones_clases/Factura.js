e088d6b2a48e0abfbc7f6443641545b3cffb721e
Atolsada <Atolsadag@gmail.com>
2018-07-19 10:10:36 +0200

    Creacion Funciones para hacer full clases

---
+++ 06Facturas_Web_ES6/Funciones_clases/Factura.js
@@ -0,0 +1,8 @@
+import {} from './Empresa.js'
+export function Factura (tipoIVA, formaPago) {
+    this.cliente = ''
+    this.elementos = []
+    this.importeTotal = 0
+    this.tipoIVA = tipoIVA
+    this.formaPago = formaPago
+}
