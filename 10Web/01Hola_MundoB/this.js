10e25ce790052c73b04e65cf9e33b03a2834361a
Atolsada <Atolsadag@gmail.com>
2018-07-23 11:01:24 +0200

    Creacion y explicacion This

--- 10Web/01Hola_MundoB/this.js
+++ 10Web/01Hola_MundoB/this.js
@@ -11,5 +11,31 @@ pepe.saludar()
 // jose.saludar() 
 // obj = obj
 
-// saludarFuera = pepe.saludar
-// obj = obj
+// let saludarFuera = pepe.saludar
+// pepe.saludar()
+// saludarFuera()
+
+
+// THIS, COBRA VALOR EN EL MOMENTO EN EL QUE SE USA NO EN EL QUE SE DEFINE,AL NO SER QUE USEMOS UN ARROW FUNCTION
+// QUE THIS QUEDA FIJADO PARA SIEMPRE EN EL MOMENTO EN EL QUE DA DEFINIMOS
+
+// saludar : () => {
+//     console.log. (`Hola, soy ${this.nombre}`);} <--- Undefined
+// }
+
+
+let saludarFuera = pepe.saludar
+
+pepe.saludar()
+saludarFuera()
+saludarFuera.call(pepe)
+
+/* setTimeout(() =>{
+    console.log(`Salgo al segundo`)
+},1000) */
+
+setTimeout (pepe.saludar,3000)
+//Aparece Undefined por que la ejecuta EL SISTEMA despues de los 3 segundos. En el sistema no hay this
+setTimeout (pepe.saludar.bind(pepe),3000)
+//Oye sistema tras 3 segundos ejecuta pepe como si fuera un call(call dice cuanto vale this=pepe). 
+//En el fondo quien ejecuta es pepe
