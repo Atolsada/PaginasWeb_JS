3c4678a48c6906d582b707036f90df6143e977cc
Atolsada <Atolsadag@gmail.com>
2018-07-24 13:44:27 +0200

    Need fix - comprobar con Prof.

--- 11WebBOM/cookies/app.js
+++ 11WebBOM/cookies/app.js
@@ -1,3 +1,22 @@
-import { Cookie } from './node_modules/js-cookie/src/js.cookie.js'
+// * as Pepe para que funcione (engañamos al comando) y funciona
+import * as Pepe from './node_modules/js-cookie/src/js.cookie.js'
 
-console.log(Cookie.get())
+// ESTA es la buena, pero por algún motivo a nadie le funciona
+// import Cookies from './node_modules/js-cookie/src/js.cookie.js'
+//console.log(Cookies.get())
+
+Cookies.get('visita')
+//Cookies.set('visita', '1')
+console.log(Cookies.get())
+
+let numVisitas =  Cookies.get('NumVisitas')
+if (numVisitas) {
+     // Existe la cookie NumVisitas
+     Cookies.set('NumVisitas', numVisitas, {expires: 7})
+     console.log('Visita número', numVisitas)
+    
+} else {
+    // No existe la cookie NumVisitas
+    Cookies.set('NumVisitas', 1, {expires: 7})
+}
+
