5b30ae9747818c793f13afa933b8cd25c19b76ee
Atolsada <Atolsadag@gmail.com>
2018-07-25 10:33:57 +0200

    Buscar en google - fix

--- 11WebBOM/buscar/app.js
+++ 11WebBOM/buscar/app.js
@@ -2,6 +2,15 @@ class App {
     constructor() {
         this.inClave = document.querySelector('#inClave')
         this.btnBuscar = document.querySelector('#btnBuscar')
+
+        this.btnBuscar.addEventListener('click', () => {
+            console.log(this)
+            let clave = this.inClave.value
+            const url = 'https://www.google.es/search?q='    //almacenar las url de las busquedas
+            if (clave){ //validar el dato escrito (comprobando)
+                window.location.href = url + clave // usa la const url mas clave
+            }    
+        })
     }
 }
 
