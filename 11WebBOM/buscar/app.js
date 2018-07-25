c8f4fe2af742d5ac2e14de249ddd4487f254b775
Atolsada <Atolsadag@gmail.com>
2018-07-25 10:10:03 +0200

    Creacion de buscador

---
+++ 11WebBOM/buscar/app.js
@@ -0,0 +1,9 @@
+class App {
+    constructor() {
+        this.inClave = document.querySelector('#inClave')
+        this.btnBuscar = document.querySelector('#btnBuscar')
+    }
+}
+
+document.addEventListener('DOMContentLoaded', 
+    () => {new App()})
