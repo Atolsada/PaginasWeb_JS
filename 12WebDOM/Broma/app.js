f453bad993e9a97e90be50094fd5ec126960e019
Atolsada <Atolsadag@gmail.com>
2018-07-26 11:14:16 +0200

    Creacion de ejercicio bromas

---
+++ 12WebDOM/Broma/app.js
@@ -0,0 +1,13 @@
+
+class App {
+    constructor(){
+        let jaJa = console.dir(document.querySelector('div li'))
+        nodoJaja.forEach( 
+        (jaJa) => {Node.innerHTML = 'Te troileé xXd'})
+    }
+}
+
+let nodoJaja = document.querySelector('#jaja')
+
+document.addEventListener('DomContentLoaded', 
+() => {new ApplicationCache()})
