480f3db37dcf57269b4174da8eeb6c2683450bb3
Atolsada <Atolsadag@gmail.com>
2018-07-30 12:28:47 +0200

    END Eventos propios - para juegos

--- 13Eventos/03eventos_propios/app.js
+++ 13Eventos/03eventos_propios/app.js
@@ -1,5 +1,5 @@
 function main(){
-    console.dir(main)
+    /* console.dir(main)
     document.querySelector('#btnPulsar')
         .addEventListener('click', pulsar) // (1)
 
@@ -16,7 +16,26 @@ function main(){
         oEv.target.dispatchEvent(
             new Event('escribir', {'bubbles': true}) // (2)
         )
+    } */
+
+    document.querySelector('#btnPulsar')
+        .addEventListener('click', pulsar)
+
+    document.querySelector('main') //<--- quien ecribe es main, el boton le avisa al main
+        .addEventListener('escribir', escribir)
+
+        
+    //Para el enlace de google
+
+
+
+    function pulsar(oEv) {
+        console.dir(oEv.target)
+        oEv.target.dispatchEvent(new Event('escribir', {
+            'bubbles': true,
+          }))
     }
+
     function escribir (oEv){
         console.dir(oEv)
         document.querySelector('#output').innerHTML = "He recibido un evento 'escribir' "
